efde6fc4032204630f0e53f175746354f3f2b1c3
rammoon13 <herreraroblesramon@gmail.com>
2025-02-04 10:17:44 +0100

    javadoc main

--- src/main/java/ies/castillodeluna/Main.java
+++ src/main/java/ies/castillodeluna/Main.java
@@ -6,7 +6,16 @@ import java.sql.SQLException;
 import ies.castillodeluna.ui.Menu;
 import ies.castillodeluna.db.DatabaseManager;
 
+/**
+ * Clase principal que inicia la aplicación y gestiona la conexión con la base de datos.
+ */
 public class Main {
+
+    /**
+     * Método principal que ejecuta la aplicación.
+     *
+     * @param args Argumentos de línea de comandos (no utilizados en esta aplicación).
+     */
     public static void main(String[] args) {
         // Intenta establecer la conexión con la base de datos
         try (Connection conn = DatabaseManager.getConnection()) {
